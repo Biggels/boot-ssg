48a854d39d03aedd9b093fa7b63ff5ff769e19a1
biggels <haackra@gmail.com>
2026-05-27 21:13:00 -0400

    add leafnode with tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -21,3 +21,20 @@ class HTMLNode:
 
     def __repr__(self):
         return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag=tag, value=value, children=None, props=props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("All leaf nodes must have a value.")
+        if self.tag is None:
+            return self.value
+        html_str = f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+        return html_str
+
+    def __repr__(self):
+        return f"LeafNode(tag={self.tag}, value={self.value}, props={self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -56,4 +56,30 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(
             node.__repr__(),
             "HTMLNode(tag=a, value=moogle to google, children=None, props={'href': 'https://www.google.com', 'target': '_blank'})",
+        )
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html_value_is_none(self):
+        node = LeafNode(tag="p", value=None)
+        self.assertRaises(ValueError, node.to_html)
+
+    def test_to_html_tag_is_none(self):
+        node = LeafNode(tag=None, value="this is some untagged text")
+        self.assertEqual(node.to_html(), "this is some untagged text")
+
+    def test_to_html_p(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        self.assertEqual(node.to_html(), "<p>This is a paragraph of text.</p>")
+
+    def test_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node.to_html(), '<a href="https://www.google.com">Click me!</a>'
+        )
+
+    def test_repr(self):
+        node = LeafNode(tag="p", value="this is a paragraph")
+        self.assertEqual(
+            node.__repr__(), "LeafNode(tag=p, value=this is a paragraph, props=None)"
         )
